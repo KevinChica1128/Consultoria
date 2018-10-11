f144ff89b9532d5f4d5214ed3387a702df8585b8
KevinChica1128 <kevin.chica@correounivalle.edu.co>
2018-10-11 15:49:19 -0500

    codigo y base de datos

---
+++ script consultoria.R
@@ -0,0 +1,6 @@
+#Script Consultoria
+
+#Cargamos la base de datos
+library(readxl)
+PROPUESTA_FUNDEMERCA_1_ <- read_excel("C:/Users/KEVINSTEVEN/Desktop/UNIVALLE/CONSULTORIA/BUCANERO/PROPUESTA FUNDEMERCA (1).xlsx")
+View(PROPUESTA_FUNDEMERCA_1_)
