b55a55c096428b42e06f302af90c47795b9597eb
KevinChica1128 <kevin.chica@correounivalle.edu.co>
2018-10-19 22:06:43 -0500

    intervalos de credibilidad

--- script consultoria 1.R
+++ script consultoria 1.R
@@ -2,7 +2,7 @@
 
 #Cargamos la base de datos
 library(readxl)
-PROPUESTA_FUNDEMERCA_1_ <- read_excel("C:/Users/KEVINSTEVEN/Desktop/UNIVALLE/CONSULTORIA/BUCANERO/PROPUESTA FUNDEMERCA (1).xlsx")
+PROPUESTA_FUNDEMERCA_1_ <- read_excel("C:/Users/KEVINSTEVEN/Desktop/UNIVALLE/CONSULTORIA/BUCANERO/PROPUESTA FUNDEMERCA (2.5).xlsx")
 View(PROPUESTA_FUNDEMERCA_1_)
 datos<-as.data.frame(PROPUESTA_FUNDEMERCA_1_[,c(-1,-2)])
 row.names(datos)<-PROPUESTA_FUNDEMERCA_1_$Producor
@@ -67,3 +67,58 @@ graf1=ggplot(df,aes(cod,proporcion, ymin=inferior, ymax=superior))+
 
 x11()
 graf1
+
+#Intervalos de credibilidad con Poisson:
+library("HDInterval")
+library("TeachingDemos")
+
+#Cargamos la base de datos de los pollos muertos:
+pollosmuertos<- read_excel("GitHub/Consultoria/PROPUESTA FUNDEMERCA (2.5).xlsx")
+pollosmuertos<-as.data.frame(pollosmuertos[,c(-1,-2)])
+row.names(pollosmuertos)<-PROPUESTA_FUNDEMERCA_1_$Producor
+
+#Para el ciclo 50 del productor ALBEIRO HERNANDEZ
+L1<-rgamma(10000,pollosmuertos[1,1]+(1/2), rate=1) #Valores simulados de la aposteriori
+emp.hpd (L1, conf = 0.95 )   #Intervalo con el 95% de confianza 
+
+#Con los cuantiles
+qgamma(0.025,pollosmuertos[1,1]+(1/2), rate=1)
+qgamma(0.975,pollosmuertos[1,1]+(1/2), rate=1)
+
+#Generalización para todos los ciclos y todos los productores con función:
+Intervalos<-data.frame(matrix(NA,48,51))#matriz para almacenar los intervalos
+colnames(Intervalos)=c("cod",rep(c("Inferior","Superior"),25))
+row.names(Intervalos)=PROPUESTA_FUNDEMERCA_1_$Producor
+Intervalos[,1]=1:48
+for (j in 1:length(datos[,1])) {
+  for (i in seq(2,51,2)) {
+    if(anyNA(pollosmuertos[j,i-(i/2)])=="TRUE"){
+      is.na<-Intervalos[j,i]
+      is.na<-Intervalos[j,i+1]
+    }else{
+      L<-rgamma(10000,pollosmuertos[j,i-(i/2)]+(1/2),rate = 1)
+      Intervalos[j,i]=emp.hpd(L,0.95)[1]
+      Intervalos[j,i+1]=emp.hpd(L,0.95)[2]
+      }
+  }
+}
+
+#Generalización para todos los ciclos y todos los productores con cuantiles:
+IntervalosC<-data.frame(matrix(NA,48,51))#matriz para almacenar los intervalos
+colnames(IntervalosC)=c("cod",rep(c("Inferior","Superior"),25))
+row.names(IntervalosC)=PROPUESTA_FUNDEMERCA_1_$Producor
+IntervalosC[,1]=1:48
+for (j in 1:length(datos[,1])) {
+  for (i in seq(2,51,2)) {
+    if(anyNA(pollosmuertos[j,i-(i/2)])=="TRUE"){
+      is.na<-IntervalosC[j,i]
+      is.na<-IntervalosC[j,i+1]
+    }else{
+      L<-rgamma(10000,pollosmuertos[j,i-(i/2)]+(1/2),rate = 1)
+      IntervalosC[j,i]=qgamma(0.025,pollosmuertos[j,i-(i/2)]+(1/2), rate=1)
+      IntervalosC[j,i+1]=qgamma(0.975,pollosmuertos[j,i-(i/2)]+(1/2), rate=1)
+    }
+  }
+}
+
+
